318d759e3ef7ba9ed20bfb7ef64aca0b55c1f115
Ashwin <69374625+xxiz@users.noreply.github.com>
2025-11-24 13:23:15 -0800

    refactor: Standardize Alembic revision identifiers to numeric format

--- alembic/versions/003_add_web_auth.py
+++ alembic/versions/003_add_web_auth.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '003_add_web_auth'
-down_revision = '002_add_scan_priority'
+revision = '003'
+down_revision = '002'
 branch_labels = None
 depends_on = None
 
